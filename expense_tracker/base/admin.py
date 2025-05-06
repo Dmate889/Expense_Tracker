52748f9222e699e56afe49b63de44224c6903c55
FireWolfy89 <dmate899@gmail.com>
2025-05-06 21:57:12 +0200

    Created models, made connections

--- expense_tracker/base/admin.py
+++ expense_tracker/base/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Year, Months, Budget, Expenses
 
 # Register your models here.
+
+admin.site.register(Year)
+admin.site.register(Months)
+admin.site.register(Budget)
+admin.site.register(Expenses)
--- expense_tracker/base/models.py
+++ expense_tracker/base/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import DecimalField, CharField
+
 
 # Create your models here.
+
+class Year(models.Model):
+    year = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Months(models.Model):
+    MONTH_CHOICES = [
+        ("jan", "January"),
+        ("feb", "February"),
+        ("mar", "March"),
+        ("apr", "April"),
+        ("may", "May"),
+        ("jun", "June"),
+        ("jul", "July"),
+        ("aug", "August"),
+        ("sep", "September"),
+        ("oct", "October"),
+        ("nov", "November"),
+        ("dec", "December"),
+    ]
+
+    name = models.CharField(max_length=3, choices=MONTH_CHOICES)
+
+
+    year = models.OneToOneField(Year, related_name='months', blank=True, on_delete= models.CASCADE)
+
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return dict(self.MONTH_CHOICES).get(self.name, self.name)
+
+class Budget(models.Model):
+    month = models.OneToOneField(Months, related_name='budget', blank=True, on_delete = models.CASCADE)
+
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.amount)
+
+class Expenses(models.Model):
+    expense = DecimalField(max_digits=10, decimal_places=2)
+    category = CharField(max_length=20)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.category}: {self.expense}"
+
